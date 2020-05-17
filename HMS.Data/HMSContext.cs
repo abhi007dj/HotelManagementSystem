b0c5a26061a57c1efb2c7d2ee0beed02cd38f26c
abhinav narwariya <abhinavhhh18@gmail.com>
2020-05-18 00:42:45 +0530

    some data changes

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Data
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+    }
+}
