a22c5d17ce23ea3ba17284f3748a2b475495f64b
abhinav narwariya <abhinavhhh18@gmail.com>
2020-05-18 13:29:52 +0530

    installed ef and enabled migrations

--- HMS.Data/HMSContext.cs
+++ HMS.Data/HMSContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HMS.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,8 +10,14 @@ namespace HMS.Data
 {
     public class HMSContext : DbContext
     {
-        public HMSContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        public HMSContext() : base("HMSConnectionString")
         {
         }
+
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
     }
 }
